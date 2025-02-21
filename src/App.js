954371dde64222212ef382e7be946d7cbc8c3eb2
shitalshivale <shitalshivale18@gmail.com>
2025-02-21 11:26:14 +0530

    feat: initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Welcome to dashboard
+      </header>
+    </div>
+  );
+}
+
+export default App;
